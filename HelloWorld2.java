ba088c36784caf7567799f3e8b3c964eb99eee4a
Manoj Reddy <manoj1992@gmail.com>
2014-04-03 09:19:26 -0400

    adding new java file

---
+++ HelloWorld2.java
@@ -0,0 +1,6 @@
+public class HelloWorld2{
+    public static void main(){
+	System.out.println("I have noticed that another interesting use of
+GitHub is to store files just like Dropbox.");
+    }
+}
